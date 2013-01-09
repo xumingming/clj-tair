a6fb95b9cdf7587fc6a48778fb59a09e278d42f5
James Xu <xumingming64398966@gmail.com>
2013-01-09 13:44:40 +0800

    implement many methods && add unit test

--- src/tair/core.clj
+++ src/tair/core.clj
@@ -1,7 +1,8 @@
 (ns tair.core
   (:refer-clojure :exclude [get])
-  (:import [com.taobao.tair TairManager ResultCode]
+  (:import [com.taobao.tair TairManager ResultCode CallMode]
            [com.taobao.tair.impl.mc MultiClusterTairManager]
+           [com.taobao.tair.etc KeyValuePack]
            [com.alibaba.fastjson JSON]
            [java.net URL]
            [java.util Map List])
@@ -15,7 +16,8 @@
      (mk-tair config-id true))
   ([config-id dynamic?]
      (doto (MultiClusterTairManager.) (.setConfigID config-id)
-           (.setDynamicConfig dynamic?))))
+           (.setDynamicConfig dynamic?)
+           (.init))))
 
 (defn get
   "Query the value of the specified key from the specified namespace."
@@ -39,24 +41,26 @@
   [^TairManager tair namespace keys]
   (let [ret (.mget tair namespace (vec keys))
         ret (if (and (not (nil? ret))
-                     (not (nil? (-> ret .getValue)))
-                     (not (nil? (-> ret .getValue .getValue))))
-              (-> ret .getValue
-                  .getValue
-                  pretify-result)
-              nil)]
+                     (not (nil? (-> ret .getValue))))
+              (into {} (map #(vector (.getKey %) (.getValue %)) (.getValue ret)))
+              nil)
+        ]
     ret))
 
 (defn put
   "Put the key value pair into the specified namespace with the specified expiretime.
 
-  If 0 is specified for the expiretime, it means it will not expire"
+  If 0 is specified for the expiretime, it means it will not expire
+  If fill-cache? is true, then it will also insert the data into cache (only
+  applies when server-side support cache."
   ([tair namespace key value]
      (put tair namespace key value 0))
   ([tair namespace key value version]
      (put tair namespace key value version 0))
   ([tair namespace key value version expire-time]
-     (let [result-code (.put tair namespace key value version expire-time)
+     (put tair namespace key value version 0 true))
+  ([tair namespace key value version expire-time fill-cache?]
+     (let [result-code (.put tair namespace key value version expire-time fill-cache?)
            result-code (clojurify-result-code result-code)]
        result-code)))
 
@@ -72,38 +76,80 @@
     result-code))
 
 (defn invalid
+  "
+  callmode can be one of [:sync :async]"
   ([tair namespace key]
-     nil)
+     (invalid tair namespace key :sync))
   ([tair namespace key callmode]
-     nil))
+     (let [callmode (if (= :sync callmode)
+                      CallMode/SYNC
+                      CallMode/ASYNC)
+           result-code (.invalid tair namespace key callmode)]
+       (clojurify-result-code result-code))))
 
 (defn hide
   [tair namespace key]
-  nil)
+  (let [result-code (.hide tair namespace key)]
+    (clojurify-result-code result-code)))
 
 (defn hide-by-proxy
   ([tair namespace key]
-     nil)
+     (hide-by-proxy tair namespace key :sync))
   ([tair namespace key callmode]
-  nil))
+     (let [callmode (if (= :sync callmode)
+                      CallMode/SYNC
+                      CallMode/ASYNC)
+           result-code (.hideByProxy tair namespace key callmode)]
+       (clojurify-result-code result-code))))
 
 (defn get-hidden
   [tair namespace key]
-  nil)
+  (let [obj (.getHidden tair namespace key)
+        obj (if (and (not (nil? obj))
+                     (not (nil? (-> obj .getValue)))
+                     (not (nil? (-> obj .getValue .getValue))))
+              (-> obj .getValue
+                  .getValue
+                  pretify-result)
+              nil)]
+    obj))
 
 (defn prefix-put
-  ([tair namespace pkey skey value] nil)
-  ([tair namespace pkey skey value version] nil)
-  ([tair namespace pkey skey value version expire-time] nil))
+  ([tair namespace pkey skey value]
+     (prefix-put tair namespace pkey skey value 0))
+  ([tair namespace pkey skey value version]
+     (prefix-put tair namespace pkey skey value version 0))
+  ([tair namespace pkey skey value version expire-time]
+     (let [result-code (.prefixPut tair namespace pkey skey value version expire-time)]
+       (clojurify-result-code result-code))))
 
 (defn prefix-puts
-  [tair namespace pkey skey-value-pairs] nil)
+  [tair namespace pkey skey-value-packs]
+  (let [skey-value-packs (map (fn [key-value-pack]
+                                (case (count key-value-pack)
+                                  2 (KeyValuePack. (first key-value-pack) (second key-value-pack))
+                                  3 (KeyValuePack. (first key-value-pack) (second key-value-pack) (nth key-value-pack 2))
+                                  4 (KeyValuePack. (first key-value-pack) (second key-value-pack) (nth key-value-pack 2) (nth key-value-pack 3)))) skey-value-packs)
+        result (.prefixPuts tair namespace pkey skey-value-packs)
+        result (if (and (not (nil? result))
+                        (not (nil? (.getValue result))))
+                 (into {} (map #(vector (first %) (clojurify-result-code (second %))) (.getValue result)))
+                 {})]
+    result))
 
 (defn prefix-delete
-  [tair namespace pkey skey] nil)
+  [tair namespace pkey skey]
+  (let [result-code (.prefixDelete tair namespace pkey skey)]
+    (clojurify-result-code result-code)))
 
 (defn prefix-deletes
-  [tair namespace pkey skeys] nil)
+  [tair namespace pkey skeys]
+  (let [result (.prefixDeletes tair namespace pkey skeys)
+        result (if (and (not (nil? result))
+                        (not (nil? (.getValue result))))
+                 (into {} (map #(vector (first %) (clojurify-result-code (second %))) (.getValue result)))
+                 {})]
+    result))
 
 (defn- object-to-json [obj]
   (JSON/toJSON obj))
--- test/tair/core_test.clj
+++ test/tair/core_test.clj
@@ -1,17 +1,58 @@
 (ns tair.core_test
   (refer-clojure :exclude [get namespace])
   (:import [com.taobao.tair TairManager Result DataEntry ResultCode])
+  (:import [java.io Serializable])
   (:use [tair.core])
   (:use [clojure.test]))
 
 ;; define the mock tair
 (def tair (reify TairManager
             (get [this namespace key]
-              (let [result-code (doto (ResultCode. 0 "success"))
+              (let [result-code (ResultCode. 0 "success")
                     data-entry (doto (DataEntry.)
                                  (.setKey "foo")
                                  (.setValue "bar"))
                     ret (Result. result-code data-entry)]
+                ret))
+            (^ResultCode put [this ^int namespace ^Serializable key ^Serializable value
+                              ^int version ^int expire-time ^boolean fill-cache?]
+              (let [^ResultCode result-code (ResultCode. 0 "success")]
+                result-code))
+            (mget [this namespace keys]
+              (let [data-entries [(DataEntry. "foo" "bar")
+                                  (DataEntry. "hello" "world")]
+                    result-code (ResultCode. 0 "success")
+                    ret (Result. result-code data-entries)]
+                ret))
+            (delete [this namespace key]
+              (ResultCode. 0 "success"))
+            (invalid [this namespace key callmode]
+              (ResultCode. 0 "success"))
+            (hide [this namespace key]
+              (ResultCode. 0 "success"))
+            (hideByProxy [this namespace key callmode]
+              (ResultCode. 0 "success"))
+            (getHidden [this namespace key]
+              (let [result-code (ResultCode. 0 "success")
+                    data-entry (doto (DataEntry.)
+                                 (.setKey "foo")
+                                 (.setValue "bar"))
+                    ret (Result. result-code data-entry)]
+                ret))
+            (prefixPut [this namespace pkey skey value version expire-time]
+              (ResultCode. 0 "success"))
+            (prefixPuts [this namespace pkey skey-value-packs]
+              (let [result-code (ResultCode. 0 "success")
+                    ret (Result. result-code {"bar1" result-code
+                                              "bar2" result-code
+                                              "bar3" result-code})]
+                ret))
+            (prefixDelete [this namespace pkey skey]
+              (ResultCode. 0 "success"))
+            (prefixDeletes [this namespace pkey skeys]
+              (let [result-code (ResultCode. 0 "success")
+                    ret (Result. result-code {"bar1" result-code
+                                              "bar2" result-code})]
                 ret))))
 
 ;; define the test namespace
@@ -19,3 +60,49 @@
 
 (deftest test-get
   (is (= "bar" (get tair namespace "key"))))
+
+(deftest test-mget
+  (is (= {"foo" "bar" "hello" "world"} (mget tair namespace ["foo" "hello"]))))
+
+(deftest test-put
+  (is (= {:code 0 :message "success"} (put tair namespace "foo" "bar")))
+  (is (= {:code 0 :message "success"} (put tair namespace "foo" "bar" 0)))
+  (is (= {:code 0 :message "success"} (put tair namespace "foo" "bar" 0 0)))
+  (is (= {:code 0 :message "success"} (put tair namespace "foo" "bar" 0 0 false))))
+
+(deftest test-delete
+  (is (= {:code 0 :message "success"} (delete tair namespace "foo"))))
+
+(deftest test-invalid
+  (is (= {:code 0 :message "success"} (invalid tair namespace "foo")))
+  (is (= {:code 0 :message "success"} (invalid tair namespace "foo" :async))))
+
+(deftest test-hide
+  (is (= {:code 0 :message "success"} (hide tair namespace "foo"))))
+
+(deftest test-hide-by-proxy
+  (is (= {:code 0 :message "success"} (hide-by-proxy tair namespace "foo"))))
+
+(deftest test-get-hidden
+  (is (= "bar" (get-hidden tair namespace "key"))))
+
+(deftest test-prefix-put
+  (is (= {:code 0 :message "success"} (prefix-put tair namespace "foo" "bar" "value")))
+  (is (= {:code 0 :message "success"} (prefix-put tair namespace "foo" "bar" "value" 0)))
+  (is (= {:code 0 :message "success"} (prefix-put tair namespace "foo" "bar" "value" 0 0))))
+
+(deftest test-prefix-puts
+  (is (= {"bar1" {:code 0 :message "success"}
+          "bar2" {:code 0 :message "success"}
+          "bar3" {:code 0 :message "success"}}
+         (prefix-puts tair namespace "foo" [["bar1" "value1"]
+                                            ["bar2" "value2" 0]
+                                            ["bar3" "value3" 0 0]]))))
+
+(deftest test-prefix-delete
+  (is (= {:code 0 :message "success"} (prefix-delete  tair namespace "foo" "bar"))))
+
+(deftest test-prefix-deletes
+  (is (= {"bar1" {:code 0 :message "success"}
+          "bar2" {:code 0 :message "success"}}
+         (prefix-deletes tair namespace "foo" ["bar1" "bar2"]))))
